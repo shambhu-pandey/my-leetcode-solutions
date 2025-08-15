8df81f54b259712421a5b64bd6805afccab85e4a
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2025-08-15 07:37:47 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

---
+++ problems/problems/merge_two_sorted_lists/solution.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if(a == null) return b;
+        if(b == null) return a;
+
+        if(a.val < b.val){
+            a.next = mergeTwoLists(a.next , b);
+            return a;
+        }
+        else{
+            b.next = mergeTwoLists(a , b.next);
+            return b;
+        }
+    }
+}
