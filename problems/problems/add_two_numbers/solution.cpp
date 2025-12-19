150b7f916431c6325995c429f0bafc14f6899931
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2025-12-19 20:02:37 +0000

    Sync LeetCode submission - Add Two Numbers (cpp)

---
+++ problems/problems/add_two_numbers/solution.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+
+        int carry =0;
+        while(l1!=NULL || l2!= NULL){
+        int sum = carry;
+        if(l1) sum += l1->val;
+        if(l2) sum += l2->val;
+
+        ListNode* newnode = new ListNode(sum%10);
+        carry = sum/10;
+        
+        curr->next = newnode;
+        curr = curr->next;
+
+        if(l1) l1= l1->next;
+        if(l2) l2 =l2->next;
+        }
+
+        if(carry){
+            ListNode* newnode = new ListNode(carry);
+            curr->next = newnode;
+        }
+
+        return dummy->next;
+    }
+};
