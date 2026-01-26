a925266817430ad19b39d5ed474aa1dd18e9fb1f
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2026-01-26 09:36:09 +0000

    Sync LeetCode submission - Count Primes (cpp)

--- problems/problems/count_primes/solution.cpp
+++ problems/problems/count_primes/solution.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    // Keeping your isPrime for style (not used in final count)
-    bool isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-
     int countPrimes(int n) {
-        if (n <= 2) return 0;
 
-        vector<bool> prime(n, true);
-        prime[0] = prime[1] = false;
+        vector<bool> isprime(n);
+        for(int i = 0;  i< n; i++){
+            isprime[i] = true;
+        }
 
-        for (int i = 2; i * i < n; i++) {
-            if (prime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    prime[j] = false;
+        for(int i = 2; i*i<n ; i++){
+            if(isprime[i]==true){
+                for(int j = i*i ; j<n; j+=i){
+                    isprime[j]= false;
                 }
             }
         }
-
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) count++;
+        int count =0;
+        for(int i = 2; i<n; i++){
+            if(isprime[i]==true){
+                count++;
+            }
         }
+
         return count;
     }
 };
