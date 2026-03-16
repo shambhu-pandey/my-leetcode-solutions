abbe61fe6f2d16e1eee665c8529cf6583a97f87e
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2026-03-16 10:00:40 +0000

    Sync LeetCode submission - Number of 1 Bits (java)

---
+++ problems/problems/number_of_1_bits/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingWeight(int n) {
+        
+        int count =0;
+        while(n>0){
+           if(n%2==1){
+            count++;
+           }
+           n = n/2;
+        }
+        return count;
+
+        // int count = Integer.bitCount(n);
+        // return count;
+    }
+}
