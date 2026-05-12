3852a77735a88f262f97dfae8d8e4fa83b25dbd6
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2026-05-12 09:19:41 +0000

    Sync LeetCode submission - Majority Element (java)

--- problems/problems/majority_element/solution.java
+++ problems/problems/majority_element/solution.java
@@ -2,20 +2,7 @@ class Solution {
     public int majorityElement(int[] nums) {
         
         int n = nums.length;
-
-        for(int i =0; i<n ; i++){
-             int count =0; // reset after iteration 
-            for(int j =0; j<n; j++){
-                if(nums[i]== nums[j]){
-                    count++;
-                }
-            }
-        
-        if(count>n/2){
-          return nums[i];
-        }
-        }
-       return -1;
-
+        Arrays.sort(nums);
+        return nums[n/2];
     }
 }
