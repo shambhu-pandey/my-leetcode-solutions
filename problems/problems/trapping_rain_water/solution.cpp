edaf86da66f37e7eee11e785f27f0d2258c47597
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2026-05-12 09:14:21 +0000

    Sync LeetCode submission - Trapping Rain Water (cpp)

---
+++ problems/problems/trapping_rain_water/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left =0;
+        int right = height.size()-1;
+
+        int leftmax =height[left];
+        int rightmax=height[right];
+        int water =0;
+        while(left<right){
+               if(leftmax < rightmax){
+                left++;
+                leftmax = max(leftmax , height[left]);
+                water =  water + leftmax - height[left]; // height of left means block 
+               }
+               else{
+                right--;
+                rightmax = max(rightmax , height[right]);
+                water = water + rightmax -height[right];
+               }
+        }
+          return water;
+    }
+  
+};
