d90299ee7c3a4e26d9d5cfe50ee02b93ba09faad
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2026-05-17 15:48:13 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- problems/problems/palindrome_number/solution.cpp
+++ problems/problems/palindrome_number/solution.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-
-        if(x<0) return false; //  negative num is not a palindrome
-        long original = x;
-        long  reverseNum = 0;
-        while(x>0){
-            long lastdigit = x%10 ;
-            reverseNum = reverseNum*10+lastdigit;
-            x = x/10;
+        
+        if(x<0 || (x%10==0 && x!=0)){
+            return false;
         }
+        long long reversedNum =0;
+        int original = x;
 
-      return reverseNum == original;
+        while(x>0){
+            int lastDigit= x%10;   // last digit nikaal liye 
+            reversedNum = (reversedNum*10)+ lastDigit;
+            x /=10;
+        }
+        return reversedNum == original;
     }
 };
