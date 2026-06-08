dd575cf237842936c5fa52c579bb6b58ce558a7c
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2026-06-08 13:51:45 +0000

    Sync LeetCode submission - Rotate Array (cpp)

--- problems/problems/rotate_array/solution.cpp
+++ problems/problems/rotate_array/solution.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        vector<int> temp;
-        int n = nums.size();
-         k = k%n;
 
-
-         // store last k elements
-        for(int i = n-k;i<n;i++){
-              temp.push_back(nums[i]);
-        }
-
-           // shift remaining elements to the right
-        for(int i = n-1 ; i>=k; i--){
-            nums[i]=nums[i-k]; 
-        }
-           
-        // place temp elements at start
-        for(int i = 0 ; i<k; i++){
-            nums[i]=temp[i]; 
+    void reverse(vector<int>&nums , int start ,int end){
+        while(start<end){
+            swap(nums[start] , nums[end]);
+            start++;
+            end--;
         }
     }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
 };
+
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         k= k%n;
+
+//         vector<int>rotate;
+
+//         for(int i = n-k;i<n;i++){
+//             rotate.push_back(nums[i]);
+//         }
+//         for(int i = 0;i<n-k;i++){
+//             rotate.push_back(nums[i]);
+//         
+
+//         for(int i =0;i<n;i++){
+//             nums[i] = rotate[i];
+//         }
+//     }
+// };
